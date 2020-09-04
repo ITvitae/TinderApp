dc314e87f86cbaaed9e328738a4b2f1c98caf676
pieterbrandsen <45623682+pieterbrandsen@users.noreply.github.com>
2020-09-04 14:39:56 +0200

    Revert "Mock data push"
    This reverts commit 213becb93e8a9602ef6b43ad4ec0cc17404bd5cf.

--- TinderApp/Models/Item.cs
+++ TinderApp/Models/Item.cs
@@ -5,11 +5,7 @@ namespace TinderApp.Models
     public class Item
     {
         public string Id { get; set; }
-        public string FullName { get; set; }
-        public GenderEnum Gender { get; set; }
-        public string Age { get; set; }
-        public string City { get; set; }
-        public string Country { get; set; }
+        public string Text { get; set; }
         public string Description { get; set; }
     }
 }
