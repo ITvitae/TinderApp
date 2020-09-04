ccdd0192d2103a93a0b403bdf96fff0d25a17e98
pieterbrandsen <45623682+pieterbrandsen@users.noreply.github.com>
2020-09-04 14:44:32 +0200

    Push

--- TinderApp/Models/Item.cs
+++ TinderApp/Models/Item.cs
@@ -5,7 +5,11 @@ namespace TinderApp.Models
     public class Item
     {
         public string Id { get; set; }
-        public string Text { get; set; }
+        public string FullName { get; set; }
+        public GenderEnum Gender { get; set; }
+        public string Age { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
         public string Description { get; set; }
     }
 }
